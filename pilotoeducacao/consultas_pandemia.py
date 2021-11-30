6eea811c99d1374ffefcdf8d8036d3f1468d2b14
Pedro <tigreped@gmail.com>
2021-11-30 12:18:19 -0300

    Refactoring queries to add one highlight per line with a document index for the queries.

--- pilotoeducacao/consultas_pandemia.py
+++ pilotoeducacao/consultas_pandemia.py
@@ -33,7 +33,6 @@ consultas = [
         "primary_term": "ensino remoto emergencial",
         "secondary_terms": [
             ["equipamento"],
-            ['aluno', 'aluna', 'alunas', 'alunos'],
             ['tablet', 'mobile', 'smartphone', 'notebook']
         ]
     }, {
@@ -41,7 +40,6 @@ consultas = [
         "secondary_terms": [
             ["dados móveis"],
             ["contrato", "provedor"],
-            ['aluno', 'aluna', 'alunas', 'alunos']
         ]
     }, {
         "primary_term": "ensino",
@@ -52,7 +50,7 @@ consultas = [
     }, {
         "primary_term": "educação",
         "secondary_terms": [
-            ["ensino", "escola","remoto", "virtual"],
+            ["ensino", "escola","remota", "virtual"],
             ["internet móvel", "dados móveis", "3G", "4G", "5G"],
             ["contrato", "licitação", "fornecedor", "provedor"]
         ]
@@ -60,7 +58,7 @@ consultas = [
         "primary_term": "ensino",
         "secondary_terms": [
             ["banda larga", "fibra ótica", "ADSL"],
-            ["educação remota", "ensino remoto"]
+            ["educação remota", "remoto"]
         ]
     }, {
         "primary_term": "educação",
@@ -70,7 +68,6 @@ consultas = [
     }, {
         "primary_term": "ensino",
         "secondary_terms": [
-            ["virtual"],
             ["classroom", "Cogna", "Brainy", "Stoodi", "eduedu", "superensino"]
         ]
     }, {
@@ -103,6 +100,22 @@ consultas = [
             ["educação",  "ensino", "aprendizagem", "aprendizado", "home school", "home schooling"],
             ["internet", "em casa", "domiciliar", "à distância", "remota", "remoto", "virtual"],
             ["superensino", "Eduedu", "eduplay", "Cogna", "liber", "Brainy","Stoodi"]
+        ]
+    }, {
+        "primary_term": "educação",
+        "secondary_terms": [
+            ["positivo", "lenovo", "samsung", "LG", "HP"]
+        ]
+    }, {
+        "primary_term": "ensino remoto emergencial",
+        "secondary_terms": [
+            ["positivo", "lenovo", "samsung", "LG", "HP"]
+        ]
+    }, {
+        "primary_term": "ensino remoto emergencial",
+        "secondary_terms": [
+            ["internet", "digital"],
+            ['tablet', 'mobile', 'smartphone', 'notebook']
         ]
     }
 ]
@@ -139,6 +152,7 @@ def main():
         spreadsheet_key = planilha_2021_id
     if index_to_query == index_qd_full:
         spreadsheet_key = planilha_qd_full
+
     # define the access scope allowed to the credentials
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
     # fetch information from json file and process with defined scope
@@ -150,7 +164,7 @@ def main():
     # Para cada uma das consultas da lista de consultas:
     for consulta in consultas:
         # Lists for keeping results that will be converted to dataframe, one index per response, for all lists, representing a single row in the spreadsheets
-        index_list, primary_terms_list, secondary_terms_list, highlights_list, territory_name_list, urls_list = [], [], [], [], [], []
+        index_list, query_index_list, primary_terms_list, secondary_terms_list, highlights_list, territory_name_list, urls_list = [], [], [], [], [], [], []
         #print("Consulta : %s" % consulta)
         query = assembleIntervalQuery(consulta["primary_term"], consulta["secondary_terms"])
         start_time = time.time()
@@ -164,59 +178,85 @@ def main():
         end_time = time.time() - start_time
         if results:
             n_hits = return_hits(results)
-            print(f'→→→ Busca finalizada demorou %d segundos para retornar (%d) itens.' % (int(end_time), n_hits))
+            print(f'→→→ A busca finalizada demorou %d segundos para retornar (%d) itens.' % (int(end_time), n_hits))
             # From the returned results, extract highlights, city and urls
             highlights = return_highlights(results)
             cidades = return_territory_name(results)
             urls = return_urls(results)
 
+            index_n = 0
             for n in range(0, n_hits):
-                # Increment index and append it to list
-                index_list.append(n)
-                # Add the query's primary term to the list
-                primary_terms_list.extend([consulta['primary_term']])
-                # Add the secondary terms to the list as a list of string elements to be printed as a single cell
-                secondary_terms_list.extend([str(consulta['secondary_terms'])])
                 try:
-                    highlights_text = []
                     # Fetch the first[0] highlight for this query/terms
                     for h in highlights[n]:
-                        # Fix highlights blank chars:
-                        highlights_text.append(remove_breaks(h))
-                    # Concatenate all highlights into a single text, separated by double blank lines \n
-                    separated_highlights = "\n\n".join(highlights_text)
-                    # Add highlights to the list of highlights
-                    highlights_list.append(separated_highlights)
-                except:
-                    pass
-                try:
-                    # Add the city name to the list of cities
-                    territory_name_list.extend([cidades[n]])
-                except:
-                    pass
-                try:
-                    urls_list.extend([urls[n]])
+                        # Fix highlights blank chars with qd's toolbox remove_breaks:
+                        highlights_clean_text = remove_breaks(h)
+
+                        # LEAP: Ensure only highlights WITH at least N matching secondary terms appear
+                        n_secondary_matches = 0
+                        # And also matches the primary term
+                        primary_match = False
+
+                        # Check primary term match
+                        if highlights_clean_text.find(consulta['primary_term']) > 0:
+                            primary_match = True
+
+                        # Para cada termo de um conjunto de termos secundários, verifique se ocorre no highlight
+                        for st in consulta['secondary_terms']:
+                            # Para cada termo, verificar cada palavra
+                            for word in st:
+                                if highlights_clean_text.find(word) > 0:
+                                    # Increment counter
+                                    n_secondary_matches = n_secondary_matches + 1
+
+                        # If the current highlight has secondary_term matches, it is relevant. Add it to the list
+                        if primary_match and (n_secondary_matches > 0):
+                            # Increment index and append it to list
+                            index_list.append(index_n)
+                            index_n = index_n + 1
+
+                            # Put the current document result index to the current query to group highlights
+                            query_index_list.append(n)
+
+                            # Add the query's primary term to the list
+                            primary_terms_list.append(consulta['primary_term'])
+
+                            # Add the secondary terms to the list as a list of string elements to be printed as a single cell
+                            secondary_terms_list.append(str(consulta['secondary_terms']))
+
+                            # Add highlights to the list of highlights
+                            highlights_list.append(highlights_clean_text)
+
+                            # Add the city name to the list of cities
+                            territory_name_list.append(cidades[n])
+
+                            # Add the DO's url
+                            urls_list.append(urls[n])
+                        else:
+                            # TODO: Here we can add an empty object to be considered with 0 values when when exporting "total" value tab
+                            pass
                 except:
                     pass
 
         # Convert list of responses to dataframe
-        responses_df = pd.DataFrame( list(zip( primary_terms_list, secondary_terms_list, urls_list, highlights_list, territory_name_list)), columns=['termo_principal', 'termos_complementares', 'url', 'excerto', 'cidade'], index=index_list)
+        responses_df = pd.DataFrame( list(zip( query_index_list, primary_terms_list, secondary_terms_list, urls_list, highlights_list, territory_name_list)), columns=['indice', 'termo_principal', 'termos_complementares', 'url', 'excerto', 'cidade'], index=index_list)
 
         # Selecionar os resultados das cidades relevantes
         cidades_ref = ['Goiânia', 'Manaus', 'Rio de Janeiro']
 
         city_subset = responses_df[responses_df['cidade'].isin(cidades_ref)]
         # Caso haja algum resultado
-        if city_subset.size > 0:
+        if (city_subset.count()[0] > 0):
             # Sort by city name and secondary terms and turn into list from df
             city_subset = (city_subset.sort_values('termos_complementares'))
             city_subset = (city_subset.sort_values('cidade'))
+            city_subset = (city_subset.sort_values('indice'))
             # The list of elements to be uploaded to the sheets
             city_subset_list = city_subset.values.tolist()
 
             print(f'Updating spreadsheets with %d new items' % len(city_subset))
             try:
-                # Upload in shards of up to 500 items # TODO: may not be necessary
+                # Upload in shards of up to 500 items
                 N_ITEMS_BY_SHARD = 500
                 n_items = len(city_subset_list)
                 # Treat cases where the amount of results to be added is greater than the shard limit
@@ -228,24 +268,24 @@ def main():
                     for n in range(1, n_shards):
                         shard_items = city_subset_list[lower_limit:upper_limit]
                         print("Elements to add: %d " % len(shard_items))
-                        print("Elements: %s" % shard_items)
                         # Execute update operation to the spreadsheets with provided parameters of range, valueInputOption, insertDataOption and the city subset list as values.
-                        res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'resultados!A2:E2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': shard_items}).execute()
-                        print(f'Response: %s' % res)
+                        res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'resultados!A2:F2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': shard_items}).execute()
+                        print(f'Spreadsheets Response: \n%s' % json.dumps(res))
+                        # Increment limits for next round
                         lower_limit = lower_limit + N_ITEMS_BY_SHARD
                         upper_limit = upper_limit + N_ITEMS_BY_SHARD
                         # Impose upper limit to n_items to avoid index overflowing
                         if upper_limit > n_items:
                             upper_limit = n_items
                 else:
-                    res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'resultados!A2:E2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': city_subset_list}).execute()
-                    print(f'Response: %s' % res)
+                    res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'resultados!A2:F2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': city_subset_list}).execute()
+                    print(f'Spreadsheets Response: \n%s' % json.dumps(res))
                 pass
             except:
                 print(f'Something went wrong with Spreasheets upload: %s.' %  sys.exc_info()[0])
                 pass
 
-            # New dataframe to hold the lines with totals
+            # New object to create dataframe to hold the lines with total counts
             counts_obj = {
                 f'termo_principal': [],
                 f'termos_complementares': [],
@@ -253,21 +293,56 @@ def main():
                 'total': []
             }
 
+            # secondary terms as string
+            sec_terms_str_list = ""
+            for st in consulta["secondary_terms"]:
+                sec_terms_str_list = sec_terms_str_list + " " + str(st)
+            print("→→→ Secondary terms: %s" % sec_terms_str_list)
+
             for c in cidades_ref:
                 subcity = city_subset[city_subset['cidade'] == c]
+                # Number of rows for this dataframe
                 n_itens = subcity.count()[0]
                 counts_obj['termo_principal'].append(consulta["primary_term"])
-                counts_obj['termos_complementares'].append(str(consulta["secondary_terms"]))
+                counts_obj['termos_complementares'].append(sec_terms_str_list)
                 counts_obj['cidade'].append(c)
                 counts_obj['total'].append(n_itens)
+
+            # Generate dataframe from object
             counts_df = pd.DataFrame(counts_obj)
+            print("→→ Dataframe: %s" % counts_df)
+
             # Adiciona à segunda aba da planilha um resumo com as contagens para cada consulta, por cidade
             result_totals = counts_df.values.tolist()
             print(f'Updating spreadsheets totals with %d new lines' % len(result_totals))
             try:
                 # Execute update operation to the spreadsheets with provided parameters of range, valueInputOption, insertDataOption and the city subset list as values.
                 res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'total!A2:D2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': result_totals}).execute()
-                print(f'Response: %s' % res)
+                print(f'Spreadsheets Response: \n%s' % json.dumps(res))
+                pass
+            except:
+                print(f'Something went wrong with Spreasheets upload: %s.' %  sys.exc_info()[0])
+                pass
+            pass
+        # Case the city has no results, should still add empty results to display
+        else:
+            try:
+                primary_term_str = consulta["primary_term"]
+                # Secondary terms aux
+                secondary_terms_str = str(consulta['secondary_terms'])
+                # New object to create dataframe to hold the lines with total counts
+                counts_obj = {
+                    "termo_principal": [primary_term_str, primary_term_str, primary_term_str],
+                    "termos_complementares": [secondary_terms_str, secondary_terms_str, secondary_terms_str ],
+                    "cidade": cidades_ref,
+                    "total": [0, 0, 0]
+                }
+                counts_df = pd.DataFrame(counts_obj)
+                # Adiciona à segunda aba da planilha um resumo com as contagens para cada consulta, por cidade
+                result_totals = counts_df.values.tolist()
+                # Execute update operation to the spreadsheets with provided parameters of range, valueInputOption, insertDataOption and the city subset list as values.
+                res = service.spreadsheets().values().append(spreadsheetId=spreadsheet_key, range=f'total!A2:D2', valueInputOption=f'USER_ENTERED', insertDataOption="INSERT_ROWS", body= {f'values': result_totals}).execute()
+                print(f'Spreadsheets Response: \n%s' % json.dumps(res))
                 pass
             except:
                 print(f'Something went wrong with Spreasheets upload: %s.' %  sys.exc_info()[0])
@@ -320,7 +395,7 @@ def assembleIntervalQuery(primary_term: str, secondary_terms: []):
                 "source_text" : {
                     "all_of" : {
                         "intervals" : [],
-                        "max_gaps" : 100
+                        "max_gaps" : 50
                     }
                 }
             }
@@ -329,8 +404,9 @@ def assembleIntervalQuery(primary_term: str, secondary_terms: []):
         "size": 10000,
         "highlight": {
             "type" : "unified",
-            "fragment_size": 500,
-            "number_of_fragments" : 8,
+            "fragment_size": 1500,
+            "number_of_fragments" : 1000,
+            "order": "score",
             "fields": {
                 "source_text": {}
             }
@@ -347,7 +423,7 @@ def assembleIntervalQuery(primary_term: str, secondary_terms: []):
         # Add it to the assembled_query obj
     assembled_query["query"]["intervals"]["source_text"]["all_of"]["intervals"].extend([primary_term_block])
 
-    # Aqui, se há múltiplos termos deve-se gerar um block any_of a selecionar um dos dois termos. Se há apenas um termo, adiciona-se um match
+    # Aqui, se há múltiplos termos deve-se gerar um bloco any_of para selecionar um dos termos. Se há apenas um termo, adiciona-se apenas um bloco match
     for terms in secondary_terms:
         match = None
         # Any_of block
@@ -358,7 +434,7 @@ def assembleIntervalQuery(primary_term: str, secondary_terms: []):
                 "intervals" : []
                 }
             }
-            # Multiple options to choose one from, add to new any_of block. Ex.: ["wireless", "sem fio"]
+            # Multiple options to choose one from, add new any_of block. Ex.: ["wireless", "sem fio"]
             # Aqui, vamos adicionar individualmente os sinônimos
             for term in terms:
                 split_term = term.split()
